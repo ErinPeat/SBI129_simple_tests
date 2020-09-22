ead6f4efbb5d4cfc64f01bed40a3806d82e045cb
ErinPeat <erin.peat@nhs.net>
2020-09-22 11:49:17 +0100

    First commit of file and test file

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a simple function to get testing started
+
+def func(x):
+    x = x + 1
+    return x
+
+#I need to figure out why I really need this and if I do!
+if __name__ == '__main__':
+    unittest.main()
+    
---
+++ test_main.py
@@ -0,0 +1,10 @@
+import unittest
+
+#This is a simple test to test the simple function
+#Importing the function for use in the test
+from main import func
+
+#Test the function
+class TestSimpleFunction(unittest.TestCase):
+    def test_answer(self):
+        self.assertEqual(func(3), 5)
